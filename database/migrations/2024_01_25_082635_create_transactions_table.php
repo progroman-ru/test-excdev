7faea989363ddec2987475ddee82715fbca59a54
Roman Shipilov <r.shipilov@gmail.com>
2024-01-25 13:25:53 +0300

    Миграции для balances, transactions

---
+++ database/migrations/2024_01_25_082635_create_transactions_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('balance_id')->index();
+            $table->decimal('amount', 13, 2);
+            $table->string('description');
+            $table->timestamp('created_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
